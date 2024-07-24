50a3e1aba3496bef6f5bbdd745f0d189d50998b0
gonblas <gonzagabino@gmail.com>
2024-07-24 15:52:01 -0300

    Fastfetch added and change colors in polybar

--- .config/rofi/applets/bin/wifi-menu.sh
+++ .config/rofi/applets/bin/wifi-menu.sh
@@ -1,38 +1,79 @@
 #!/usr/bin/env bash
 
-notify-send "Getting list of available Wi-Fi networks..."
-# Get a list of available wifi connections and morph it into a nice-looking list
-wifi_list=$(nmcli --fields "SECURITY,SSID" device wifi list | sed 1d | sed 's/  */ /g' | sed -E "s/WPA*.?\S/ /g" | sed "s/^--/ /g" | sed "s/  //g" | sed "/--/d")
+# Inicia un escaneo de SSIDs disponibles
+nmcli dev wifi rescan
 
-connected=$(nmcli -fields WIFI g)
-if [[ "$connected" =~ "enabled" ]]; then
-	toggle="󰖪  Disable Wi-Fi"
-elif [[ "$connected" =~ "disabled" ]]; then
-	toggle="󰖩  Enable Wi-Fi"
-fi
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Use rofi to select wifi network
-chosen_network=$(echo -e "$toggle\n$wifi_list" | uniq -u | rofi -dmenu -i -selected-row 1 -p "Wi-Fi SSID: " )
-# Get name of connection
-read -r chosen_id <<< "${chosen_network:3}"
+CAMPOS=SSID,SECURITY
+POSICION=3
+YOFF=25
+XOFF=-5
+FUENTE="JetBrains 10"
 
-if [ "$chosen_network" = "" ]; then
-	exit
-elif [ "$chosen_network" = "󰖩  Enable Wi-Fi" ]; then
-	nmcli radio wifi on
-elif [ "$chosen_network" = "󰖪  Disable Wi-Fi" ]; then
-	nmcli radio wifi off
+if [ -r "$DIR/config" ]; then
+    source "$DIR/config"
+elif [ -r "$HOME/.config/rofi/applets/bin/wifi-menu" ]; then
+    source "$HOME/.config/rofi/wifi"
 else
-	# Message to show when connection is activated successfully
-  	success_message="You are now connected to the Wi-Fi network \"$chosen_id\"."
-	# Get saved connections
-	saved_connections=$(nmcli -g NAME connection)
-	if [[ $(echo "$saved_connections" | grep -w "$chosen_id") = "$chosen_id" ]]; then
-		nmcli connection up id "$chosen_id" | grep "successfully" && notify-send "Connection Established" "$success_message"
-	else
-		if [[ "$chosen_network" =~ "" ]]; then
-			wifi_password=$(rofi -dmenu -p "Password: " )
-		fi
-		nmcli device wifi connect "$chosen_id" password "$wifi_password" | grep "successfully" && notify-send "Connection Established" "$success_message"
-    fi
+    echo "ADVERTENCIA: ¡archivo de configuración no encontrado! Usando valores predeterminados."
 fi
+
+LISTA=$(nmcli --fields "$CAMPOS" device wifi list | sed '/^--/d')
+ANCHO=$(($(echo "$LISTA" | head -n 1 | awk '{print length($0); }')+10))
+NUM_LINEAS=$(echo "$LISTA" | wc -l)
+CONOCIDAS=$(nmcli connection show)
+ESTADO_CON=$(nmcli -fields WIFI g)
+
+SSID_ACTUAL=$(LANGUAGE=C nmcli -t -f active,ssid dev wifi | awk -F: '$1 ~ /^yes/ {print $2}')
+
+if [[ ! -z $SSID_ACTUAL ]]; then
+    LINEA_ALTA=$(echo  "$(echo "$LISTA" | awk -F "[  ]{2,}" '{print $1}' | grep -Fxn -m 1 "$SSID_ACTUAL" | awk -F ":" '{print $1}') + 1" | bc )
+fi
+
+if [ "$NUM_LINEAS" -gt 8 ] && [[ "$ESTADO_CON" =~ "enabled" ]]; then
+    NUM_LINEAS=8
+elif [[ "$ESTADO_CON" =~ "disabled" ]]; then
+    NUM_LINEAS=1
+fi
+
+if [[ "$ESTADO_CON" =~ "enabled" ]]; entonces
+    CAMBIO="desactivar"
+elif [[ "$ESTADO_CON" =~ "disabled" ]]; entonces
+    CAMBIO="activar"
+fi
+
+SELECCION=$(echo -e "$CAMBIO\nmanual\n$LISTA" | uniq -u | rofi -dmenu -p "SSID Wi-Fi: " -lines "$NUM_LINEAS" -a "$LINEA_ALTA" -location "$POSICION" -yoffset "$YOFF" -xoffset "$XOFF" -font "$FUENTE" -width -"$ANCHO")
+SSID=$(echo "$SELECCION" | sed  's/\s\{2,\}/\|/g' | awk -F "|" '{print $1}')
+
+if [ "$SELECCION" = "manual" ] ; entonces
+    MANUAL=$(echo "ingrese el SSID de la red (SSID,contraseña)" | rofi -dmenu -p "Entrada Manual: " -font "$FUENTE" -lines 1)
+    PASS=$(echo "$MANUAL" | awk -F "," '{print $2}')
+
+    if [ "$PASS" = "" ]; entonces
+        nmcli dev wifi con "$MANUAL"
+    else
+        nmcli dev wifi con "$MANUAL" password "$PASS"
+    fi
+
+elif [ "$SELECCION" = "activar" ]; entonces
+    nmcli radio wifi on
+
+elif [ "$SELECCION" = "desactivar" ]; entonces
+    nmcli radio wifi off
+
+else
+
+    if [ "$SSID" = "*" ]; entonces
+        SSID=$(echo "$SELECCION" | sed  's/\s\{2,\}/\|/g' | awk -F "|" '{print $3}')
+    fi
+
+    if [[ $(echo "$CONOCIDAS" | grep "$SSID") = "$SSID" ]]; entonces
+        nmcli con up "$SSID"
+    else
+        if [[ "$SELECCION" =~ "WPA2" ]] || [[ "$SELECCION" =~ "WEP" ]]; entonces
+            WIFIPASS=$(echo "si la conexión está guardada, presione enter" | rofi -dmenu -p "contraseña: " -lines 1 -font "$FUENTE" )
+        fi
+        nmcli dev wifi con "$SSID" password "$WIFIPASS"
+   
+
