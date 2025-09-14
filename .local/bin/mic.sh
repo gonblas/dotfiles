e41786a5f54e473ff0dba9eb50a8b8efd610214e
Gonzalo Blasco <gonzagabino@gmail.com>
2025-09-14 20:48:41 -0300

    Mic in polybar fix

--- .local/bin/mic.sh
+++ .local/bin/mic.sh
@@ -1,20 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
-mic_source=$(pactl info | awk -F': ' '/Default Source:/ {print $2}')
-
-if [ "$1" == "1" ]; then
-  pactl set-source-mute "$mic_source" toggle
-fi
-
-mic_status=$(pactl list sources | awk -v mic_source="$mic_source" '
-  $0 ~ "^Source #" {in_source=0}
-  $0 ~ "Name: " mic_source {in_source=1}
-  in_source && /Mute:/ {print $2; exit}
-')
-
-if [ "$mic_status" == "yes" ]; then
-  echo -e "󰍭"
+# Verificar estado y mostrar icono
+if pactl get-source-mute @DEFAULT_SOURCE@ | grep -q "yes"; then
+    echo "󰍭"   # Micrófono muteado
 else
-  echo -e "󰍬"
+    echo "󰍬"   # Micrófono activo
 fi
 
